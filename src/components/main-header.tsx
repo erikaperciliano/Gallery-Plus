df2c6ceae7241c38d182500ab0cbfac362d24906
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-09-19 18:30:09 -0300

    feat: adds PhotosSearch Component

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -3,6 +3,8 @@ import Logo from '../assets/images/galeria-plus-full-logo.svg?react';
 import { Link } from "react-router";
 import cx from 'classnames';
 import Button from "./button";
+import PhothosSearch from "./photos-search";
+import Divider from "./divider";
 
 interface MainHeaderProps extends React.ComponentProps<typeof Container> {}
 
@@ -16,6 +18,9 @@ export default function MainHeader({className, ...props}: MainHeaderProps) {
             <Link to='/'>
                 <Logo className="h-5" />
             </Link>
+
+            <PhothosSearch />
+            <Divider orientation="vertical" className="h-10"/>
 
             <div className="flex items-center gap-3">
                 <Button>Nova foto</Button>
