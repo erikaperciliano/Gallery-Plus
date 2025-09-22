180a2743a9bf4a7225fa5f8b0c6cd73cf7d85107
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-09-22 15:51:01 -0300

    feat: adds mock of the add photo dialog

--- src/components/main-header.tsx
+++ src/components/main-header.tsx
@@ -5,6 +5,7 @@ import cx from 'classnames';
 import Button from "./button";
 import PhothosSearch from "./photos-search";
 import Divider from "./divider";
+import PhotoNewDialog from "../contexts/albums/components/photo-new-dialog";
 
 interface MainHeaderProps extends React.ComponentProps<typeof Container> {}
 
@@ -23,7 +24,9 @@ export default function MainHeader({className, ...props}: MainHeaderProps) {
             <Divider orientation="vertical" className="h-10"/>
 
             <div className="flex items-center gap-3">
-                <Button>Nova foto</Button>
+                <PhotoNewDialog trigger={
+                    <Button>Nova foto</Button>
+                }/>
                 <Button variant="secondary">Criar álbum</Button>
             </div>
         </Container>
