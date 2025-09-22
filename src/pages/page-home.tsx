d1a6de6af6315728ff959d79f4fe5ed138e3621e
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-09-22 13:52:09 -0300

    feat: adds mock of the albums filter

--- src/pages/page-home.tsx
+++ src/pages/page-home.tsx
@@ -1,14 +1,41 @@
 import Container from "../components/container";
+import AlbumsFilter from "../contexts/albums/components/albums-filter";
 import PhotoList from "../contexts/photos/components/photo-list";
 
 export default function PageHome() {
     return(
         <Container>
+            <AlbumsFilter 
+                albums={[
+                    {id: '3421', title: 'Album 1'},
+                    {id: '123', title: 'Album 2'},
+                    {id: '456', title: 'Album 2'},
+                ]} 
+                className="mb-9"
+            />
             <PhotoList 
                 photos={[
-                    
+                    {
+                        id: '123',
+                        title: 'PageHome',
+                        imageId: 'portrait-tower.png',
+                        albums: [
+                            {id: '3421', title: 'Album 1'},
+                            {id: '123', title: 'Album 2'},
+                            {id: '456', title: 'Album 2'},
+                        ],
+                    },
+                    {
+                        id: '321',
+                        title: 'PageHome',
+                        imageId: 'portrait-tower.png',
+                        albums: [
+                            {id: '3421', title: 'Album 1'},
+                            {id: '123', title: 'Album 2'},
+                            {id: '456', title: 'Album 2'},
+                        ],
+                    },
                 ]} 
-                loading
             />
         </Container>
     )
