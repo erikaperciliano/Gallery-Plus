a060c9426df2d566ca204050a485bc095254629b
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-09-22 14:53:25 -0300

    feat: adds mock of the details page: list of selectable albums

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -6,6 +6,7 @@ import Skeleton from "../components/skeleton";
 import PhotosNavigator from "../contexts/photos/components/photos-navigator";
 import ImagePreview from "../components/image-preview";
 import Button from "../components/button";
+import AlbumsListSelectable from "../contexts/albums/components/albums-list-selectable";
 
 export default function PagePhotoDetails() {
     const {id} = useParams();
@@ -26,7 +27,7 @@ export default function PagePhotoDetails() {
         <Container>
             <header className="flex items-center justify-between gap-8 mb-8">
                 {!isLoadingPhoto ? (
-                    <Text variant="heading-large">{photo?.title}</Text>
+                    <Text as="h2" variant="heading-large">{photo?.title}</Text>
                 ): (
                     <Skeleton className="w-48 h-8" />
                 )}
@@ -34,7 +35,7 @@ export default function PagePhotoDetails() {
                 <PhotosNavigator loading={isLoadingPhoto}/>
             </header>
 
-            <div className="grid grid-cols-[21rem] gap-24">
+            <div className="grid grid-cols-[21rem_1fr] gap-24">
                 <div className="space-y-3">
                     {!isLoadingPhoto ? (
                         <ImagePreview 
@@ -51,7 +52,16 @@ export default function PagePhotoDetails() {
                     ): (
                         <Skeleton className="w-20 h-10"/>
                     )}
+                </div>
 
+                <div className="py-3">
+                    <Text as="h3" variant="heading-medium" className="mb-6">Álbuns</Text>
+
+                    <AlbumsListSelectable photo={photo} albums={[
+                        {id: '3421', title: 'Album 1'},
+                        {id: '123', title: 'Album 2'},
+                        {id: '456', title: 'Album 2'},
+                    ]}/>
                 </div>
             </div>
         </Container>
