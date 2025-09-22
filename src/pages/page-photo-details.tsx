15e2c03b169238e09caf516a21122e090084d701
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-09-22 14:19:53 -0300

    feat: adds a mock of the Header Page, Image and Delete Button

--- src/pages/page-photo-details.tsx
+++ src/pages/page-photo-details.tsx
@@ -1,14 +1,59 @@
 import { useParams } from "react-router";
 import Text from "../components/text";
+import Container from "../components/container";
+import type { Photo } from "../contexts/photos/models/photo";
+import Skeleton from "../components/skeleton";
+import PhotosNavigator from "../contexts/photos/components/photos-navigator";
+import ImagePreview from "../components/image-preview";
+import Button from "../components/button";
 
 export default function PagePhotoDetails() {
     const {id} = useParams();
 
+    const isLoadingPhoto = false;
+    const photo = {
+        id: '123',
+        title: 'PageHome',
+        imageId: 'portrait-tower.png',
+        albums: [
+            {id: '3421', title: 'Album 1'},
+            {id: '123', title: 'Album 2'},
+            {id: '456', title: 'Album 2'},
+        ],
+    } as Photo;
+
     return(
-        <>
-            <Text>Página de detalhes da foto!</Text>
-            <hr/>
-            <Text>ID da foto: {id}</Text>
-        </>
+        <Container>
+            <header className="flex items-center justify-between gap-8 mb-8">
+                {!isLoadingPhoto ? (
+                    <Text variant="heading-large">{photo?.title}</Text>
+                ): (
+                    <Skeleton className="w-48 h-8" />
+                )}
+
+                <PhotosNavigator loading={isLoadingPhoto}/>
+            </header>
+
+            <div className="grid grid-cols-[21rem] gap-24">
+                <div className="space-y-3">
+                    {!isLoadingPhoto ? (
+                        <ImagePreview 
+                            src={`/images/${photo?.imageId}`}
+                            title={photo?.title}
+                            imageClassName="h-[21rem]"
+                        />
+                    ): (
+                        <Skeleton className="h-[21rem]"/>
+                    )}
+
+                    {!isLoadingPhoto ? (
+                        <Button variant="destructive">Excluir</Button>
+                    ): (
+                        <Skeleton className="w-20 h-10"/>
+                    )}
+
+                </div>
+            </div>
+        </Container>
     )
 }
