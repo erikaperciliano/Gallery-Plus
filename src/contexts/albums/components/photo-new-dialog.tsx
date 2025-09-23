d5bb76baed7a3a451dcd5b097725d8cb5707727d
Erika Perciliano <erikaperciliano.developer@gmail.com>
2025-09-23 12:29:40 -0300

    feat: selects albums to photo

--- src/contexts/albums/components/photo-new-dialog.tsx
+++ src/contexts/albums/components/photo-new-dialog.tsx
@@ -27,11 +27,26 @@ export default function PhotoNewDialog({trigger}: PhotoNewDialogProps) {
     const file = form.watch('file');
     const fileSource = file?.[0] ? URL.createObjectURL(file[0]) : undefined;
 
+    const albumsIds = form.watch('albumsIds')
+
     useEffect(() => {
         if(!modalOpen) {
             form.reset();
         }
     },[modalOpen, form])
+    
+    function handleToggleAlbum(albumId: string) {
+        const albumsIds = form.getValues('albumsIds')
+        const albumsSet = new Set(albumsIds)
+
+        if(albumsSet.has(albumId)) {
+            albumsSet.delete(albumId)
+        } else {
+            albumsSet.add(albumId)
+        }
+
+        form.setValue('albumsIds', Array.from(albumsSet))
+    }
 
     function handleSubmit(payload: PhotoNewFormSchema) {
         console.log(payload)
@@ -82,9 +97,10 @@ export default function PhotoNewDialog({trigger}: PhotoNewDialogProps) {
                                 {!isLoadingAlbums && albums.length > 0 && albums.map(album => (
                                     <Button
                                         key={album.id}
-                                        variant="ghost"
+                                        variant={albumsIds?.includes(album.id) ? "primary" : "ghost"}
                                         size="sm"
                                         className="truncate"
+                                        onClick={() => handleToggleAlbum(album.id)}
                                     >
                                         {album.title}
                                     </Button>
